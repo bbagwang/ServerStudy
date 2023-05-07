8899f0a6deb8c0b30dbc61365b217ac145a1b31c
BBAGWANG <bbagwang@gmail.com>
2023-05-07 16:12:09 +0900

    Kernel Object Event Sync Example 1

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,7 +1,56 @@
 ﻿#include "pch.h"
 #include <iostream>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <Windows.h>
+
+mutex m;
+queue<int32> q;
+HANDLE handle;
+
+void Producer()
+{
+	while (true)
+	{
+		{
+			unique_lock<mutex> lock(m);
+			q.push(100);
+		}
+
+		::SetEvent(handle);
+
+		this_thread::sleep_for(chrono::milliseconds(1000));
+	}
+}
+
+void Consumer()
+{
+	while (true)
+	{
+		::WaitForSingleObject(handle, INFINITE);
+
+		unique_lock<mutex> lock(m);
+		if (q.empty() == false)
+		{
+			int32 data = q.front();
+			q.pop();
+			cout << data << endl;
+		}
+	}
+}
 
 int main()
 {
-    HelloWorld();
+	// 커널 오브젝트
+	// Usage Count
+
+	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*bManualReset*/, FALSE/*bInitialState*/, NULL);
+	thread t1(Producer);
+	thread t2(Consumer);
+
+	t1.join();
+	t2.join();
+
+	::CloseHandle(handle);
 }
