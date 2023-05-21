10516e8eaed67a446f81ffc3e5ef3acb504cf65f
BBAGWANG <bbagwang@gmail.com>
2023-05-22 01:43:44 +0900

    Thread Local Storage Example 1

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,7 +1,30 @@
 ﻿#include "pch.h"
 #include <iostream>
+#include <thread>
+
+thread_local int32 LThreadID = 0;
+
+void ThreadMain(const int32 InLThreadID)
+{
+	LThreadID = InLThreadID;
+
+	while (true)
+	{
+		std::cout << "Thread ID: " << LThreadID << std::endl;
+		this_thread::sleep_for(1s);
+	}
+}
 
 int main()
 {
-    HelloWorld();
+	vector<thread> Threads;
+	for (int32 i = 1; i <= 10; ++i)
+	{
+		Threads.emplace_back(ThreadMain, i);
+	}
+
+	for (int32 i = 1; i <= 10; ++i)
+	{
+		Threads[i].join();
+	}
 }
