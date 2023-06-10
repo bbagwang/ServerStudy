020e7362c87ececbd79f578cb387f532d6e91c70
BBAGWANG <bbagwang@gmail.com>
2023-06-10 21:04:00 +0900

    Thread Manager

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,7 +1,24 @@
 ﻿#include "pch.h"
-#include <iostream>
+#include "CorePch.h"
+#include "ThreadManager.h"
+
+CoreGlobal Core;
+
+void ThreadMain()
+{
+	while (true)
+	{
+		cout << "Hello I'm Thread #" << LThreadId << endl;
+		this_thread::sleep_for(1s);
+	}
+}
 
 int main()
 {
-    HelloWorld();
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch(ThreadMain);
+	}
+
+	GThreadManager->Join();
 }
---
+++ ServerCore/CoreMacro.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#pragma region CRASH
+
+#define CRASH(cause)							\
+{												\
+	uint32* crash = nullptr;					\
+	__analysis_assume(crash != nullptr);		\
+	*crash = 0xDEADBEEF;						\
+}												\
+
+#define ASSERT_CRASH(expr)						\
+{												\
+	if(!(expr))									\
+	{											\
+		CRASH("ASSERT_CRASH");					\
+		__analysis_assume(Expr);				\
+	}											\
+}												\
+
+#pragma endregion CRASH
