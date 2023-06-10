9eb8c32d1dc639d8c74e0db54a2169aeacde4e8a
BBAGWANG <bbagwang@gmail.com>
2023-06-10 20:24:59 +0900

    Lock Free Queue #2
    Using Counters for Implementing Lock Free Queue.
    
    It needs to have secure the head and tail for the multi-threaded push & pop scenario.
    
    NodeCounter has an externalCountRemaining variable that checks the node is being used in both push & pop.
    
    If the externalCountRemaining is 0. there's no more race.
    It is controlled by FreeExternalCount() to decrease it.

--- GameServer/ConcurrentQueue.h
+++ GameServer/ConcurrentQueue.h
@@ -48,15 +48,71 @@ private:
 template <typename T>
 class LockFreeQueue
 {
+	struct Node;
+
+	struct CountedNodePtr
+	{
+		int32 externalCount; //참조권
+		Node* ptr = nullptr;
+	};
+
+	struct NodeCounter
+	{
+		uint32 internalCount : 30; //참조권 반환 관련
+		uint32 externalCountRemaining : 2; //Push & Pop 다중 참조권 관련
+	};
+
 	struct Node
 	{
-		shared_ptr<T> data;
-		Node* next = nullptr;
+		Node()
+		{
+			NodeCounter newCount;
+			newCount.internalCount = 0;
+			newCount.externalCountRemaining = 2;
+			count.store(newCount);
+
+			next.ptr = nullptr;
+			next.externalCount = 0;
+		}
+
+		void ReleaseRef()
+		{
+			NodeCounter oldCounter = count.load();
+
+			while (true)
+			{
+				NodeCounter newCounter = oldCounter;
+				--newCounter.internalCount;
+
+				//끼어들 수 있음
+				if (count.compare_exchange_strong(oldCounter, newCounter))
+				{
+					//건드리는 애가 아무도 없을 때
+					if (newCounter.internalCount == 0 && newCounter.externalCountRemaining == 0)
+						delete this;
+					
+					break;
+				}
+			}
+		}
+
+		atomic<T*> data;
+		atomic<NodeCounter> count;
+		CountedNodePtr next;
 	};
 
 public:
 	// 빈 데이터를 하나 두고 처리 하는 방식으로 만들자.
-	LockFreeQueue() : _head(new Node), _tail(_head) {}
+	LockFreeQueue()
+	{
+		CountedNodePtr node;
+		node.ptr = new Node;
+		node.externalCount = 1;
+
+		//Head 와 Tail 의 Count 는 따로 따로 관리됨
+		_head.store(node);
+		_tail.store(node);
+	}
 
 	//Copy 방지
 	LockFreeQueue(const LockFreeQueue&) = delete;
@@ -64,54 +120,115 @@ public:
 
 	void Push(const T& Value)
 	{
-		shared_ptr<T> newData(make_shared<T>(Value));
-		Node* dummy = new Node();
+		unique_ptr<T> newData = make_unique<T>(Value);
 
-		Node* oldTail = _tail;
-		oldTail->data->swap(newData);
-		oldTail->next = dummy;
-		_tail = dummy;
+		CountedNodePtr dummy;
+		dummy.ptr = new Node;
+		dummy.externalCount = 1;
 
-		// 1. Dummy Node 를 만든다.
-		// [New Dummy]
-		// [Data]->[Data]->[Dummy]
-		// [Head]          [Tail]
+		CountedNodePtr oldTail = _tail.load();
+		// oldTail 의 data 는 nullptr.
+		// 기존의 Dummy 를 가르키고 있기 때문. (위에 있는 dummy 아님)
 
-		// 2. oldTail 에 현재 Tail 이 가르키는 Dummy Node 를 가져와 정보를 넣는다.
-		// [Data]->[Data]->[Dummy]
-		// [Head]          [Tail]
-		
-		// oldTail = [Dummy] (_tail)
-		// [Dummy] 에 데이터 입력.
-		// [Dummy] 의 다음을 [New Dummy] 로 설정.
-		// [Dummy]->[New Dummy]
+		while (true)
+		{
+			//참조권 획득 (externalCount 를 현시점 기준 +1 한 애가 이김)
+			IncreaseExternalCount(_tail, oldTail);
 
-		// 3. Tail 을 [New Dummy] 로 설정.
-		// [Data]->[Data]->[Dummy]->[New Dummy]
-		// [Head]                   [Tail]
+			//소유권 획득 (data 를 먼저 교환한 애가 이김)
+			T* oldData = nullptr;
+			if (oldTail.ptr->data.compare_exchange_strong(oldData, newData.get()))
+			{
+				//Push 상태에서 oldTail 조작중에 Pop 이 일어나거나 해서 oldTail 이 바뀌었을 수 있음.
+				//다른 스레드에서 oldTail의 데이터를 삭제하거나 변경된 경우 문제가 됨.
+				//그래서 Push 중간에 Pop 하거나 Pop 중에 Push 를 하는 경우 처리를 위해 NodeCounter 의 externalCountRemaining 이 사용됨.
+				//externalCountRemaining 이 0 이 된 경우, Push 와 Pop 의 경쟁이 모두 끝났다는 의미.
+				//공유하는 Node 데이터에 대한 레퍼런스 카운팅을 따로 한다고 보면 됨.
+
+				oldTail.ptr->next = dummy;
+				oldTail = _tail.exchange(dummy);
+				
+				FreeExternalCount(oldTail);
+
+				newData.release(); //데이터에 대한 unique_ptr 의 소유권 포기.
+
+				break;
+			}
+
+			// 여기서 부터 소유권 경쟁 패배한 경우...
+			oldTail.ptr->ReleaseRef();
+		}
 	}
 
 	shared_ptr<T> TryPop()
 	{
-		Node* oldHead = PopHead();
-		if (oldHead == nullptr)
-			return shared_ptr<T>();
+		CountedNodePtr oldHead = _head.load();
 
-		shared_ptr<T> res(oldHead->data);
-		delete oldHead;
+		while (true)
+		{
+			//참조권 획득 (externalCount 를 현시점 기준 +1 한 애가 애김)
+			IncreaseExternalCount(_head, oldHead);
 
-		return res;
+			Node* ptr = oldHead.ptr;
+			if (ptr == _tail.load().ptr)
+			{
+				ptr->ReleaseRef();
+				return shared_ptr<T>();
+			}
+
+			//소유권 획득 (head = ptr->next)
+			if (_head.compare_exchange_strong(oldHead, ptr->next))
+			{
+				//exchange 로 하면 버그 생김. load 로 하면 괜찮음.
+				//T* res = ptr->data.exchange(nullptr);
+				T* res = ptr->data.load();
+				FreeExternalCount(oldHead);
+				return shared_ptr<T>(res);
+			}
+
+			//소유권 획득 실패한 애가 뒷정리를 한다.
+			ptr->ReleaseRef();
+		}
 	}
 
 private:
-	Node* PopHead()
+	static void IncreaseExternalCount(atomic<CountedNodePtr>& counter, CountedNodePtr& oldCounter)
 	{
-		Node* oldHead = _head;
-		if (oldHead == _tail)
-			return nullptr;
+		while (true)
+		{
+			CountedNodePtr newCounter = oldCounter;
+			++newCounter.externalCount;
 
-		_head = oldHead->next;
-		return oldHead;
+			if (counter.compare_exchange_strong(oldCounter, newCounter))
+			{
+				oldCounter.externalCount = newCounter.externalCount;
+				break;
+			}
+		}
+	}
+
+	static void FreeExternalCount(CountedNodePtr& oldNodePtr)
+	{
+		Node* ptr = oldNodePtr.ptr;
+		const int32 countIncrease = oldNodePtr.externalCount - 2;
+
+		NodeCounter oldCounter = ptr->count.load();
+
+		while (true)
+		{
+			NodeCounter newCounter = oldCounter;
+			--newCounter.externalCountRemaining;
+			newCounter.internalCount += countIncrease;
+
+			if (ptr->count.compare_exchange_strong(oldCounter, newCounter))
+			{
+				//건드리는 애가 아무도 없을 때
+				if (newCounter.internalCount == 0 && newCounter.externalCountRemaining == 0)
+					delete ptr;
+
+				break;
+			}
+		}
 	}
 
 private:
@@ -121,6 +238,6 @@ private:
 	// [Data]->[Data]->[Data]->[Dummy]
 	// [Head]                  [Tail]
 
-	Node* _head = nullptr;
-	Node* _tail = nullptr;
+	atomic<CountedNodePtr> _head;
+	atomic<CountedNodePtr> _tail;
 };
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,7 +4,7 @@
 #include "ConcurrentQueue.h"
 #include "ConcurrentStack.h"
 
-LockQueue<int32> q;
+LockFreeQueue<int32> q;
 LockFreeStack<int32> s;
 
 void Push()
@@ -12,7 +12,7 @@ void Push()
     while (true)
     {
         int32 value = rand() % 100;
-        s.Push(value);
+        q.Push(value);
 
         //!!! sleep 이 없이, 너무 빠르게 Push 하면 Pop이 느려서 데이터가 쌓이게 된다.
         //!!! 그래서 Memory Leak 가 발생할 수 있음. 다만, 이 경우는 일반적이지 않은 경우라고 가정해야함.
@@ -24,7 +24,7 @@ void Pop()
 {
     while (true)
     {
-        auto data = s.TryPop();
+        auto data = q.TryPop();
         if (data)
         {
 			cout << *data << endl;
@@ -34,12 +34,6 @@ void Pop()
 
 int main()
 {
-    shared_ptr<int32> ptr;
-    const bool bIsLockFree = std::atomic_is_lock_free(&ptr);
-
-    // !!! 머신에 따라 다르겠지만, 대부분의 머신에서 shared_ptr 은 lock free 방식으로 동작하지 않는다.
-    // !!! 가짜 Lock Free 인 상태이다!
-
     thread t1(Push);
     thread t2(Push);
     thread t3(Pop);
